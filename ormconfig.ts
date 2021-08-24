cbad2206b8d00fd7ae2d81343adb6c1fc881b796
Jin <love.you.jin.longguo@outlook.com>
2021-08-24 20:00:29 +0800

    env change

--- ormconfig.ts
+++ ormconfig.ts
@@ -6,16 +6,17 @@ const connectionOptions: ConnectionOptions = {
     entitiesDir: 'src/packages/database/models',
     migrationsDir: 'src/packages/database/migrations',
   },
-  database: config.DB.NAME,
+  //database: config.DB.NAME,
   entities: ['src/packages/database/models/*.ts'],
-  host: config.DB.HOST,
+  //host: config.DB.HOST,
   logging: false,
   migrations: ['src/packages/database/migrations/*.ts'],
-  password: config.DB.PASSWORD,
-  port: config.DB.PORT,
+  //password: config.DB.PASSWORD,
+  //port: config.DB.PORT,
   synchronize: false,
   type: 'postgres',
-  username: config.DB.USER,
+  //username: config.DB.USER,
+  url: process.env.DATABASE_URL?process.env.DATABASE_URL:`postgres://${config.DB.USER}:${config.DB.PASSWORD}@${config.DB.HOST}:${config.DB.PORT}/${config.DB.NAME}`
 }
 
 module.exports = connectionOptions
