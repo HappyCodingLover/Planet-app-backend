f2801d85ef1567be73ff40cf624a8269754d5f68
Jin <love.you.jin.longguo@outlook.com>
2021-09-08 02:16:35 +0800

    /brands/add

--- src/packages/api/resources/brands/controller.ts
+++ src/packages/api/resources/brands/controller.ts
@@ -8,36 +8,15 @@ export const brands = async (req: Request, res: Response, next: NextFunction): P
   return res.status(httpStatus.OK).send({ success: true, message: 'success', data: brands })
 }
 
-// export const addProduct = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
-//   const { title, price, categoryId, description, urls } = req.body
-//   try {
-//     const result = await getConnection()
-//       .createQueryBuilder()
-//       .insert()
-//       .into(Products)
-//       .values([
-//         {
-//           name_fr: title,
-//           price: price,
-//           categoriesSubs_id: categoryId,
-//         },
-//       ])
-//       .returning('id')
-//       .execute()
-//     const arr = await Promise.all(
-//       urls.map((url: any) => {
-//         return {
-//           product_id: result.identifiers[0].id,
-//           name: '',
-//           url: url.url,
-//         }
-//       }),
-//     )
-//     await getConnection().createQueryBuilder().insert().into(ProductImages).values(arr).execute()
-
-//     return res.status(httpStatus.OK).send({ success: true, message: 'success' })
-//   } catch (error) {
-//     console.error(error)
-//     return res.status(httpStatus.OK).send({ success: false, message: 'failed adding product' })
-//   }
-// }
+export const addBrand = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+  const data = req.body
+  const exists = await getConnection()
+    .getRepository(Brands)
+    .createQueryBuilder('brands')
+    .where('brands.name = :name', data)
+    .getCount()
+  let result
+  if (!exists) result = await getConnection().createQueryBuilder().insert().into(Brands).values([data]).execute()
+  const brands = await getConnection().createQueryBuilder(Brands, 'brands').orderBy('brands.name', 'ASC').getMany()
+  return res.status(httpStatus.OK).send({ success: true, message: 'success', data: brands })
+}
--- src/packages/api/resources/brands/index.ts
+++ src/packages/api/resources/brands/index.ts
@@ -1,8 +1,9 @@
 import Router from 'express-promise-router'
-import { brands } from '~/packages/api/resources/brands/controller'
+import { brands, addBrand } from '~/packages/api/resources/brands/controller'
 
 const router = Router()
 
 router.route('/').get(brands)
+router.route('/add').post(addBrand)
 
 export default router
