5b3103fcecb43e1b48be8dd381fc63f67162e503
Moonshot Factory <71419620+HappyCodingLover@users.noreply.github.com>
2021-11-16 21:15:21 +0800

    SOAP webServices added

--- src/packages/api/resources/delivery/controller.ts
+++ src/packages/api/resources/delivery/controller.ts
@@ -4,6 +4,8 @@ import * as path from 'path'
 import { exec } from 'child_process'
 import { Orders } from '~/packages/database/models/order'
 import { getConnection } from 'typeorm'
+import axios from 'axios'
+import { parse } from 'fast-xml-parser'
 
 export const mondialRelay = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
   const { countryCode, postcode } = req.body
@@ -60,4 +62,42 @@ export const createShipment = async (req: Request, res: Response, next: NextFunc
       }
     },
   )
+}
+
+export const searchMondialRelays = async (req: Request, res: Response): Promise<any> => {
+  const { createHash } = await import('crypto')
+  const { countryCode, postcode } = req.body
+  const security = createHash('md5').update('CC224ZXL' + countryCode + postcode + 'N1mXUzVq').digest('hex')
+  console.log('__security', security.toUpperCase())
+  // const security = md5('CC224ZXL' + countryCode + postcode + 'N1mXUzVq')
+  const data = `<?xml version="1.0" encoding="utf-8"?>\r\n<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">\r\n  <soap:Body>\r\n    <WSI2_RecherchePointRelaisAvancee xmlns="http://www.mondialrelay.fr/webservice/">\r\n      <Enseigne>CC224ZXL</Enseigne>\r\n      <Pays>${countryCode}</Pays>\r\n      <Ville></Ville>\r\n      <CP>${postcode}</CP>\r\n      <Latitude></Latitude>\r\n      <Longitude></Longitude>\r\n      <Taille></Taille>\r\n      <Poids></Poids>\r\n      <Action></Action>\r\n      <DelaiEnvoi></DelaiEnvoi>\r\n      <RayonRecherche></RayonRecherche>\r\n      <TypeActivite></TypeActivite>\r\n      <NACE></NACE>\r\n      <Security>${security.toUpperCase()}</Security>\r\n    </WSI2_RecherchePointRelaisAvancee>\r\n  </soap:Body>\r\n</soap:Envelope>`
+  const config = {
+    // method: 'post',
+    // url: 'https://api.mondialrelay.com/Web_Services.asmx',
+    headers: {
+      SOAPAction: '"http://www.mondialrelay.fr/webservice/WSI2_RecherchePointRelaisAvancee"',
+      'Content-Type': 'text/xml; charset=utf-8',
+      Cookie: 'JSESSSIONID=658741433.1.776787680.2083203584',
+    },
+    data: data,
+  }
+  let response: any
+  try {
+    response = await axios.post('https://api.mondialrelay.com/Web_Services.asmx', data, config)
+    console.log(
+      parse(response.data)['soap:Envelope']['soap:Body']?.WSI2_RecherchePointRelaisAvanceeResponse
+        ?.WSI2_RecherchePointRelaisAvanceeResult?.ListePR?.ret_WSI2_sub_PointRelaisAvancee,
+    )
+  } catch (error) {
+    console.error(error)
+  }
+
+  return res
+    .status(httpStatus.OK)
+    .send({
+      success: true,
+      message: 'success',
+      data: parse(response.data)['soap:Envelope']['soap:Body']?.WSI2_RecherchePointRelaisAvanceeResponse
+        ?.WSI2_RecherchePointRelaisAvanceeResult?.ListePR?.ret_WSI2_sub_PointRelaisAvancee,
+    })
 }
--- src/packages/api/resources/delivery/index.ts
+++ src/packages/api/resources/delivery/index.ts
@@ -1,9 +1,10 @@
 import Router from 'express-promise-router'
-import { mondialRelay, createShipment } from '~/packages/api/resources/delivery/controller'
+import { mondialRelay, createShipment, searchMondialRelays } from '~/packages/api/resources/delivery/controller'
 
 const router = Router()
 
 router.route('/mondialRelay').post(mondialRelay)
 router.route('/createShipment').post(createShipment)
+router.route('/search-mondialRelays').post(searchMondialRelays)
 
 export default router
